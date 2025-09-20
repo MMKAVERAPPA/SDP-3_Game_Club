5ce2083dac46f2dd765f51fc2dd05cc493e21b50
manveethjain8 <manveethjain@gmail.com>
2025-09-20 12:17:42 +0530

    Added server url

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080", // 🔗 update later if needed
+    baseURL: "https://sdp-3-game-club.onrender.com",
 });
 
 // Example interceptor for JWT (future use)
